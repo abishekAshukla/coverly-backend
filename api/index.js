e0405f1936a4bfd84264bdf20a41172c186231a3
abhishek <abhishe2002shukla007@gmail.com>
2023-10-05 13:34:19 +0530

    changes made suggested by vercel express docs

--- api/index.js
+++ api/index.js
@@ -1,6 +1,6 @@
 const express = require('express')
-const { errorHandler } = require('./middleware/errorHandler')
-const connectDB = require('./config/db')
+const { errorHandler } = require('../middleware/errorHandler')
+const connectDB = require('../config/db')
 const dotenv = require('dotenv').config()
 const cors = require('cors')
 const port = process.env.PORT || 5000
@@ -12,10 +12,10 @@ app.use(express.json())
 app.get('/node', (req, res) => {
   res.send('coverly backend')
 })
-app.use('/api/products', require('./routes/productsRoutes'))
-app.use('/api/brands', require('./routes/brandRoutes'))
-app.use('/api/users', require('./routes/userRoutes'))
-app.use('/api/payment', require('./routes/paymentsRoutes'))
+app.use('/api/products', require('../routes/productsRoutes'))
+app.use('/api/brands', require('../routes/brandRoutes'))
+app.use('/api/users', require('../routes/userRoutes'))
+app.use('/api/payment', require('../routes/paymentsRoutes'))
 app.use(errorHandler)
 
 app.listen(port, () => {
